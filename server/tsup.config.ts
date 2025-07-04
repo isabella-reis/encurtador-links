d226cd7e5414a7400e73a0ca1690c71dbe5a3838
Isabella Reis <isabellanery15@hotmail.com>
2025-07-04 19:31:49 -0300

    update tsup config

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/**/*.ts'],
+	entry: [
+		'src/env.ts',
+		'src/shared/either.ts',
+		'src/app/services/**/*.ts',
+		'src/infra/http/**/*.ts',
+		'src/infra/db/index.ts',
+		'src/infra/db/schemas/**/*.ts',
+		'src/infra/storage/**/*.ts',
+	],
 	clean: true,
-	format: 'esm',
+	format: ['esm'],
 	outDir: 'dist',
 })
